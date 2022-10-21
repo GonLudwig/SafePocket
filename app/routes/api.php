7dd8948442e8a00d391213dd4fa438086a02711e
GlaucoStarling <glaucox.x@gmail.com>
2022-10-21 11:02:24 -0300

    Add routes and rulesRequest

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::controller(UserController::class)->group(function () {
+    Route::post('users', 'store');
+    Route::get('users/{user}', 'show');
+    Route::put('users/{user}', 'update');
+    Route::delete('users/{user}', 'destroy');
 });
